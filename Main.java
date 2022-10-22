92333250b7d82dd5213ff422262b1341670f4213
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-22 18:58:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+  Scanner sc1 = new Scanner (System.in);
+  int num1, num2;
+    System.out.print("Insira um número: ");
+    num1 = sc1.nextInt();
+    System.out.print("Insira mais um número: ");
+    num2 = sc1.nextInt();
+  }
+}
